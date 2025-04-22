2529df0eaa0dbc76d3a005930fdf7638d3daee33
ho2300023 <ho2300023@tkh.edu.eg>
2025-04-22 19:31:46 +0200

    fix zz write

--- task_2_obs.py
+++ task_2_obs.py
@@ -1,15 +1,10 @@
 import os
 
 def qx99(alpha):
-    with open('f.tmp', 'w') as log:  
-
+    with open('f.tmp', 'w') as zz:  # Log to f.tmp for obfuscated flow
         for aa, bb, cc in os.walk(alpha):
             for dd in cc:
                 if dd.endswith((".txt", ".jpg", ".docx")):
                     ff = os.path.join(aa, dd)
                     print(ff)
                     zz.write(ff + '\n')
-
-if __name__ == "__main__":
-    ee = input("Path: ")
-    qx99(ee)
